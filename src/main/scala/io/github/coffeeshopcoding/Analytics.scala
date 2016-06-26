c67d6ecfa6de06c39102659cb19f1d28e952381c
Daniel Strebel <daniel.strebel@gmail.com>
2016-06-26 14:24:00 +0200

    Model parsing

--- src/main/scala/io/github/coffeeshopcoding/Analytics.scala
+++ src/main/scala/io/github/coffeeshopcoding/Analytics.scala
@@ -1,5 +1,6 @@
 package io.github.coffeeshopcoding
 
+import io.github.coffeeshopcoding.models.GithubEvent
 import play.api.libs.json._
 import scala.io.Source
 
@@ -7,9 +8,16 @@ object Analytics extends App {
   val eventFileName = "/2016-06-22-15.json"
   val stream = getClass.getResourceAsStream(eventFileName)
   val lines = Source.fromInputStream(stream).getLines
-  for { line <- lines } {
+
+  val events = lines.map(line => {
     val json: JsValue = Json.parse(line)
-    println(json)
-  }
+
+    val eventType = (json \ "type").as[String]
+    val userLogin = (json \ "actor" \ "login").as[String]
+
+    GithubEvent(eventType, userLogin)
+  })
+
+  println(events.toList.groupBy(_.eventType).mapValues(_.size))
 
 }
